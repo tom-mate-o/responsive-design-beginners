0fedb628ae330c4b872a82dd42b7b128b909fad2
tom-mate-o <132936694+tom-mate-o@users.noreply.github.com>
2024-06-07 13:34:04 +0200

    topnavigation hamburger menu js

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,56 @@
-console.log('Hello world!');
+const main = document.querySelector('#main');
+const footer = document.querySelector('#footer');
+const btnOpen = document.querySelector('#btnOpen');
+const btnClose = document.querySelector('#btnClose');
+const menuTopNav = document.querySelector('#menuTopNav');
+const overlay = document.querySelector('#overlay');
+const breakpoint = window.matchMedia('(width < 43.75em)');
+
+const setupTopNav = () => {
+  if (breakpoint.matches) {
+    // console.log('is mobile');
+    menuTopNav.setAttribute('inert', '');
+  } else {
+    // console.log('is tablet/desktop');
+    menuTopNav.removeAttribute('inert');
+  }
+};
+
+setupTopNav();
+
+btnOpen.addEventListener('click', openMobileMenu);
+btnClose.addEventListener('click', closeMobileMenu);
+
+breakpoint.addEventListener('change', () => {
+  //   console.log('change breakpoint');
+  setupTopNav();
+});
+
+function openMobileMenu() {
+  //   console.log('run open mobile menu');
+  btnOpen.setAttribute('aria-expanded', 'true');
+  menuTopNav.removeAttribute('inert');
+  main.setAttribute('inert', '');
+  footer.setAttribute('inert', '');
+  menuTopNav.style.transitionDuration = '400ms';
+  overlay.style.transitionDuration = '400ms';
+  bodyScrollLock.disableBodyScroll(menuTopNav);
+  btnClose.focus();
+}
+
+function closeMobileMenu() {
+  //   console.log('run close mobile menu');
+  btnOpen.setAttribute('aria-expanded', 'false');
+  menuTopNav.setAttribute('inert', '');
+  main.removeAttribute('inert');
+  footer.removeAttribute('inert');
+  bodyScrollLock.enableBodyScroll(menuTopNav);
+  btnOpen.focus();
+
+  setTimeout(() => {
+    menuTopNav.removeAttribute('style');
+    overlay.removeAttribute('style');
+  }, 400);
+}
+
+// console.log(breakpoint);
